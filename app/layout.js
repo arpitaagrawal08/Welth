67b52aeaacd09e16569e69dd43d6461df10d3389
arpitaagrawal08 <168635973+arpitaagrawal08@users.noreply.github.com>
2025-06-17 00:56:21 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,38 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header  from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+const inter=Inter({subsets:["latin"]})
+
+
+export const metadata = {
+  title: "Welth",
+  description: "Manage your finances, Your way",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${inter.className}`}
+      >
+        {/*header*/}
+        <Header/>
+        <main className="min-h-screen"> {children}</main>
+       
+        {/*footer*/}
+        <footer className="bg-blue-50 py-12">
+           <div className="container mx-auto px-4 text-center text-gray-600">
+          <p>
+            Made by: Arpita Agrawal
+          </p>
+        </div>
+        </footer>
+       
+      </body>
+    </html>
+        </ClerkProvider>
+
+  );
+}
