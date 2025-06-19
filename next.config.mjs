33d8e419be3e09e69148d32f54b2bc8d887564c8
arpitaagrawal08 <168635973+arpitaagrawal08@users.noreply.github.com>
2025-06-19 14:58:47 +0530

    email& receipt scanner

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ images:{
             hostname:'randomuser.me',
         },
     ],
-}
+},
+experimental:{
+    serverActions:{
+        bodySizeLimit:"5mb",
+    },
+},
 };
 export default nextConfig;
